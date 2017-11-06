9fcab5304d51dfde838896d7ee9e23fd111eaa1b
= <joeyscarim@gmail.com>
2017-11-05 19:25:52 -0700

    fixed ng-prod errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+     apiKey: "AIzaSyDURLn-II17DztsSM-Ilvg63JAt8OozIxM",
+    authDomain: "bootstrap-machine.firebaseapp.com",
+    databaseURL: "https://bootstrap-machine.firebaseio.com",
+    projectId: "bootstrap-machine",
+    storageBucket: "bootstrap-machine.appspot.com",
+    messagingSenderId: "252376714310"
+  }
 };
